c5b307337afe20bcdd2e42bc70dffa99cf58386a
luizfelipefarias <luizfelipefariasbraz@gmail.com>
2025-09-16 15:31:48 -0300

    Primeira atualização

---
+++ oxefood-api-fukushima/src/main/java/br/com/ifpe/oxefood/OxefoodApiFukushimaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiFukushimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiFukushimaApplication.class, args);
+	}
+
+}
